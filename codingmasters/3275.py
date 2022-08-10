9bc158c13f2a834778f514fead97eac0eb584e57
김시연 <pennya6@hs.ac.kr>
2022-08-10 09:43:03 +0900

    ~3273

--- codingmasters/3275.py
+++ codingmasters/3275.py
@@ -1,12 +1,3 @@
 a=input()
-# while len(a):
-#     if "'" in a:
-#         b=a.split("'")
-#         a=r"\'".join(b)
-#         print(a)
-#     if "\\"in a:
-#         b=a.split("\\")     
-#         print(r"\\".join(b))
-#     break
-
-    
+a=a.replace("\\","\\\\").replace("\"","\\\"").replace("\'","\\'")
+print(a)
